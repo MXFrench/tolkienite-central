63d1b7df0cc3266b49d3943f92243ce46fdd8a97
MXFrench <169309621+MXFrench@users.noreply.github.com.>
2025-07-28 14:52:01 -0700

    Update home page styles. Build navigation component.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import { useState } from 'react'
+import { pages } from '../pages/Home';
+import { NavLink } from 'react-router';
+import { IoClose } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const navLinks = [
+    {
+      id: 0,
+      name: "Home",
+      path: "/",
+    },
+    ...pages,
+  ];
+
   return (
-    <div className="fixed top-0 right-0">
-      Navigation
-    </div>
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 h-screen w-screen z-20 bg-nav-bg grid place-content-center"
+          onClick={() => setIsOpen(false)}
+        >
+          <ul className='flex flex-col gap-4 items-center'>
+            {navLinks.map(({id, name, path}) => (
+              <li key={id} className='text-2xl sm:text-4xl font-thin p-2 hover:bg-nav-highlight transition cursor-pointer text-nav-link'>
+                <NavLink to={path}
+                  className={({ isActive }) => isActive && "font-semibold"}
+                >{name}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      <button className='fixed z-30 top-4 right-4 size-12 bg-bg-secondary rounded-full text-2xl flex items-center justify-center cursor-pointer hover:shadow-lg transition'
+        onClick={() => setIsOpen(prev => !prev)}>
+        {isOpen ? <IoClose /> : <IoMenu />}
+      </button>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 
 import { Link } from 'react-router';
 
-const pages = [
+export const pages = [
   {
     id: 1,
     name: "Quote Generator",
@@ -30,17 +30,17 @@ const pages = [
 
 const Home = () => {
   return (
-    <div className="grid place-content-center grid-cols-[80rem] min-h-screen gap-8">
-      <h1 className="text-4xl font-bold tracking-tight">Welcome, Tolkienite!</h1>
-      <p className='text-2xl tracking-wide'>
-        Journey through Middle-Earth with us and enjoy fun and random content from the Lord of the Rings lore and franchise, powered by <a href="https://the-one-api.dev" className="text-links">the-one-api.dev</a>. Adventure awaits!
+    <div className="grid place-content-center grid-cols-(--container-auto-xl) min-h-screen gap-8 py-8 sm:px-4">
+      <h1 className="text-4xl 2xl:text-5xl font-bold tracking-tight">Welcome, Tolkienite!</h1>
+      <p className='text-xl 2xl:text-2xl tracking-wide'>
+        Journey through Middle-Earth with us and enjoy fun and random content from the Lord of the Rings lore and franchise, powered by <a href="https://the-one-api.dev" target="_blank" className="text-links">the-one-api.dev</a>. Adventure awaits!
       </p>
 
       <div className="grid sm:grid-cols-2 gap-8">
         {pages.map(({id, name, description, path}) => (
-          <div className="bg-bg-secondary p-8 space-y-4" key={id}>
-            <h3 className='text-3xl'>{name}</h3>
-            <p className='text-xl italic'>{description}</p>
+          <div className="bg-bg-secondary p-4 xl:p-8 space-y-4" key={id}>
+            <h3 className='text-2xl xl:text-3xl'>{name}</h3>
+            <p className='text-lg xl:text-xl italic'>{description}</p>
             <Link to={path} className='text-links'>View page</Link>
           </div>
         ))}
